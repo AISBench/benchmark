7355dcb3c40bc3a56a946e2b5e246a23ee7e7200
SJTUyh <yh_silence@alumni.sjtu.edu.cn>
2026-02-25 11:27:39 +0800

    fix custom config

--- ais_bench/configs/lmm_exmaple/multi_device_run_qwen_image_edit.py
+++ ais_bench/configs/lmm_exmaple/multi_device_run_qwen_image_edit.py
@@ -5,7 +5,7 @@ with read_base():
     from ais_bench.benchmark.configs.summarizers.example import summarizer
     from ais_bench.benchmark.configs.datasets.gedit.gedit_gen_0_shot_llmjudge import gedit_datasets
 
-device_list = [0, 1, 2, 3]
+device_list = [0] # [0, 1, 2, 3]
 
 datasets = []
 models = []
@@ -18,14 +18,17 @@ for i in device_list:
     model_config['device_kwargs']['device_id'] = i
     models.append(model_config)
 
-    dataset_config = {k: v for k, v in gedit_datasets[0].items()}
-    dataset_config['abbr'] = f"{dataset_config['abbr']}-{i}"
-    dataset_config['split_count'] = len(device_list)
-    dataset_config['split_index'] = i
-    datasets.append(dataset_config)
+    dataset_configs = []
+    for dataset in gedit_datasets:
+        dataset_config = {k: v for k, v in dataset.items()}
+        dataset_config['abbr'] = f"{dataset_config['abbr']}-{i}"
+        dataset_config['split_count'] = len(device_list)
+        dataset_config['split_index'] = i
+        dataset_configs.append(dataset_config)
+    datasets.extend(dataset_configs)
 
     # 关键：为每个设备创建一个独立的 model-dataset 组合
     model_dataset_combinations.append({
         'models': [model_config],      # 只包含当前模型
-        'datasets': [dataset_config]   # 只包含当前数据集
+        'datasets': dataset_configs   # 只包含当前数据集
     })
