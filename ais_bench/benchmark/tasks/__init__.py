c8f749ccc04f7165c2f21ac31817f0461c40b81d
GaoHua <1484391106@qq.com>
2026-04-05 07:13:41 +0000

    add vbench eval task

--- ais_bench/benchmark/tasks/__init__.py
+++ ais_bench/benchmark/tasks/__init__.py
@@ -1,3 +1,4 @@
 from ais_bench.benchmark.tasks.openicl_eval import *  # noqa: F401, F403
 from ais_bench.benchmark.tasks.openicl_infer import *  # noqa: F401, F403
 from ais_bench.benchmark.tasks.openicl_api_infer import OpenICLApiInferTask
+from ais_bench.benchmark.tasks.vbench_eval import VBenchEvalTask  # noqa: F401
