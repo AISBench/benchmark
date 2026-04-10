1c33d30e5177efdebdf3740f93e01fc1f0674598
zhanggaohua@huawei.com <GaoHua>
2026-04-10 20:01:18 +0800

    adapter swebench eval

--- ais_bench/benchmark/summarizers/__init__.py
+++ ais_bench/benchmark/summarizers/__init__.py
@@ -2,3 +2,4 @@
 from ais_bench.benchmark.summarizers.default import DefaultSummarizer  # noqa: F401
 from ais_bench.benchmark.summarizers.default_subjective import DefaultSubjectiveSummarizer  # noqa: F401
 from ais_bench.benchmark.summarizers.default_perf import DefaultPerfSummarizer # noqa: F401
+from ais_bench.benchmark.summarizers.swebench import SWEBenchSummarizer  # noqa: F401
