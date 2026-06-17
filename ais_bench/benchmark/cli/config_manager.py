87f2d44e29c38d605f9a3b6b469724ce71e9e836
SJTUyh <yh_silence@alumni.sjtu.edu.cn>
2026-06-17 16:33:16 +0800

    disable summarizer check

--- ais_bench/benchmark/cli/config_manager.py
+++ ais_bench/benchmark/cli/config_manager.py
@@ -23,7 +23,7 @@ class CustomConfigChecker:
     def check(self):
         self._check_models_config()
         self._check_datasets_config()
-        self._check_summarizer_config()
+        # self._check_summarizer_config()  # summarizer has default value, so it is not required
 
     def _check_models_config(self):
         models = self.config.get('models', [])
